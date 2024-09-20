5358c493b7ea3eed014bf907cebfaf82afa057c8
pawelek <k.pawelek@vp.pl>
2024-09-20 22:28:10 +0200

    modal styles

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -3,17 +3,20 @@ import "../styles/components/Modal.scss";
 
 const Modal = ({ title, children, onConfirm, onCancel }) => {
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <h3>{title}</h3>
+    <>
+      <div className="modal-backdrop" />
+      <div className="modal">
+        <div className="modal-content">
+          <h3>{title}</h3>
 
-        <div className="modal-content">{children}</div>
-        <div className="modal-actions">
-          <button onClick={onConfirm}>Potwierdź</button>
-          <button onClick={onCancel}>Anuluj</button>
+          <div className="modal-content">{children}</div>
+          <div className="modal-actions">
+            <button onClick={onConfirm}>Yes</button>
+            <button onClick={onCancel}>No</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
