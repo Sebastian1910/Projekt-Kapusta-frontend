7279c096a185537b8d6c835247a2893358069923
pawelek <k.pawelek@vp.pl>
2024-09-26 13:43:25 +0200

    add styles

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,14 +1,19 @@
 const Dropdown = ({ options = [], value, onChange }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
-      <option value="">Product category</option>
-      {Array.isArray(options) &&
-        options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-    </select>
+    <div className="dropdown">
+      <select
+        className="dropdown-select"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}>
+        <option value="">Product category</option>
+        {Array.isArray(options) &&
+          options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+      </select>
+    </div>
   );
 };
 
