8781329c7ad705e26749ad0261102ed1e77d3a6f
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-10-01 10:40:38 +0200

    poprawa wykresów

--- frontend/src/components/ReportsChart.jsx
+++ frontend/src/components/ReportsChart.jsx
@@ -9,8 +9,8 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 import "../styles/components/ReportsChart.scss";
-
 
 ChartJS.register(
   CategoryScale,
@@ -19,6 +19,7 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend,
+  ChartDataLabels, // Rejestracja pluginu
 );
 
 const ReportsChart = ({ transactions = [], selectedCategory }) => {
@@ -29,7 +30,9 @@ const ReportsChart = ({ transactions = [], selectedCategory }) => {
     .sort((a, b) => Math.abs(b.amount) - Math.abs(a.amount));
 
   if (categoryTransactions.length === 0) {
-    return <p className="chart-text">No data available for the selected category.</p>;
+    return (
+      <p className="chart-text">No data available for the selected category.</p>
+    );
   }
 
   const data = {
@@ -39,14 +42,11 @@ const ReportsChart = ({ transactions = [], selectedCategory }) => {
         label: "Amount",
         data: categoryTransactions.map((t) => Math.abs(t.amount)), // Ustawiamy wartości jako dodatnie
         backgroundColor: ["#FF751D", "#FED9BF", "#FED9BF"],
-        barThickness: 15, 
+        barThickness: 15,
         borderRadius: 50,
-        
       },
     ],
   };
-
-  
 
   const options = {
     responsive: true,
@@ -54,15 +54,50 @@ const ReportsChart = ({ transactions = [], selectedCategory }) => {
       legend: {
         display: false, // Ukryjemy legendę
       },
+      datalabels: {
+        display: true,
+        color: "#52555F", // Kolor etykiet
+        anchor: "end",
+        align: "top",
+        formatter: (value) => value + " UAH", // Dodanie waluty do etykiet
+        font: {
+          size: 10,
+        },
+        padding: {
+          top: 10, // Dodajemy padding od góry, aby przesunąć etykiety
+        },
+      },
     },
     scales: {
+      x: {
+        grid: {
+          display: false, // Usuwamy pionowe linie siatki
+        },
+        ticks: {
+          padding: 25, // Ustawiamy przerwę między słupkami na 25px
+        },
+      },
       y: {
         beginAtZero: true,
-        display: false, // Ukryjemy skalę Y
+        grid: {
+          display: true, // Wyświetlamy tylko poziome linie siatki
+          drawBorder: false, // Ukrywamy linię krawędzi po lewej stronie
+          color: "#e0e0e0", // Kolor poziomych linii
+        },
+        ticks: {
+          display: false, // Ukrywamy etykiety skali Y
+        },
+      },
+    },
+    layout: {
+      padding: {
+        top: 50, // Dodajemy padding od góry dla całego wykresu
+        right: 20, // Dodajemy padding z prawej strony
+        bottom: 20, // Dodajemy padding od dołu
+        left: 20, // Dodajemy padding z lewej strony
       },
     },
   };
-
   return <Bar data={data} options={options} />;
 };
 
